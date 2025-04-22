f84a4ec9ca1637a76a00540cc41905e2601bb810
deepakkasundra <81360280+deepakkasundra@users.noreply.github.com>
2025-04-22 11:51:17 +0530

    Update TicketSchema.js

--- CM/TicketSchema.js
+++ CM/TicketSchema.js
@@ -50,50 +50,9 @@ try
     deptSheet.clear();
 
     // Construct the API URL dynamically.
-    var url = Domain_name + '/bots/' + BOT_ID + '/cm/ticket-setting/settings?current=1&perPage=1000&filter=%7B%22isActive%22%3Atrue%7D';
+    var url = Domain_name + '/@@@@@@@@@@/' + BOT_ID + '/@@@@@@@@@@';
 
 
-  // var ss = SpreadsheetApp.getActiveSpreadsheet();
-  // showProgressToast(ss, 'Initializing...');
-
-  // // Read configuration from the Main sheet.
-  // var mainSheet = ss.getSheetByName('Main');
-  // var headersValues = mainSheet.getRange(1, 1, 1, mainSheet.getLastColumn()).getValues()[0];
-  // var rowIndex = 2; // Values start on row 2
-
-  // // Read BOT ID and JWT based on environment selection.
-  // var BOT_ID, jwt;
-  // if (isProd) {
-  //   BOT_ID = mainSheet.getRange(rowIndex, headersValues.indexOf('PROD BOT ID') + 1).getValue();
-  //   jwt = mainSheet.getRange(rowIndex, headersValues.indexOf('PROD JWT') + 1).getValue();
-  // var sheetName = "Ticketschema List_PROD";
-
-  // } else {
-  //   BOT_ID = mainSheet.getRange(rowIndex, headersValues.indexOf('UAT BOT ID') + 1).getValue();
-  //   jwt = mainSheet.getRange(rowIndex, headersValues.indexOf('UAT JWT') + 1).getValue();
-  // var sheetName = "Ticketschema List_UAT";
-  // }
-
-  // // Get the Dashboard Domain Name.
-  // var Domain_name = mainSheet.getRange(rowIndex, headersValues.indexOf('Dashboard Domain Name') + 1).getValue();
-
-  // // Validate required credentials.
-  // if (!BOT_ID || !jwt) {
-  //   var envname = isProd ? "Production" : "UAT";
-  //   Browser.msgBox(envname + " BOT ID or JWT Missing.", Browser.Buttons.OK);
-  //   return;
-  // }
-
-  // showProgressToast(ss, 'Clearing TicketSchema Details sheet...');
-  
-  // // Get or create the "TicketSchema Details" sheet and clear previous data.
-  // //var sheetName = "Ticketschema List";
-  // var deptSheet = ss.getSheetByName(sheetName) || ss.insertSheet(sheetName);
-  // deptSheet.clear();
-
-  // // Construct the API URL dynamically.
-  // var url = Domain_name + '/bots/' + BOT_ID + '/cm/ticket-setting/settings?current=1&perPage=1000&filter=%7B%22isActive%22%3Atrue%7D';
-  
   var options = {
     method: "get",
     muteHttpExceptions: true,
